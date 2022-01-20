290b50af4072b3f6b17a16c955891d33765fee66
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-01-20 18:17:08 -0300

    Update data

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 const holidays = [
     { date: "1/1/2022", name: "Confraternização mundial" },
-    { date: "1/3/2022", name: "Carnaval" },
+    { date: "2/28/2022", name: "Carnaval" },
     { date: "4/17/2022", name: "Páscoa" },
     { date: "4/21/2022", name: "Tiradentes" },
     { date: "5/1/2022", name: "Dia do trabalho" },
@@ -9,7 +9,8 @@ const holidays = [
     { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
     { date: "11/2/2022", name: "Finados" },
     { date: "11/15/2022", name: "Proclamação da República" },
-    { date: "12/25/2022", name: "Natal" }
+    { date: "12/25/2022", name: "Natal" },
+    { date: "12/31/2022", name: "Véspera de ano novo" }
 ];
 
 let holidaysName = '';
